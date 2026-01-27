3894fe6dc3decf9acd90a2a0ec0c2e7ed25535fa
zion tecnologia <alexcostasilva2810@gmail.com>
2026-01-27 18:43:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
 
+# O ID está na URL da sua planilha entre '/d/' e '/edit'
+ID_PLANILHA = "1sNVY3-zRHn-Oa8sGJOF5GGcfUNSNWwOb9IfcNL3mYGc" 
+URL = f"https://docs.google.com/spreadsheets/d/{ID_PLANILHA}/export?format=csv"
+
+df = pd.read_csv(URL)
+st.title("📊 Gestão de Combustível Online")
+st.write(df) # Exibe a tabela de dados
